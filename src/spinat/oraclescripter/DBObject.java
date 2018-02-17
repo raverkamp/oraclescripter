3c21e49ccdc35c011da70fc8aa42da8d9b48df5b
Roland Averkamp <roland.averkamp.0@gmail.com>
2018-02-17 23:34:36 +0100

    make DBObject a better type
    I want to use it for hashing

--- src/spinat/oraclescripter/DBObject.java
+++ src/spinat/oraclescripter/DBObject.java
@@ -1,13 +1,41 @@
 package spinat.oraclescripter;
 
-public class DBObject {
+public final class DBObject {
+    
 
     public final String type;
     public final String name;
 
     public DBObject(String type, String name) {
+        if (name ==null || type == null) {
+            throw new NullPointerException("name or type is null");
+        }
         this.type = type;
         this.name = name;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o instanceof DBObject) {
+            DBObject o2 = (DBObject) o;
+            return this.name.equals(o2.name) && this.type.equals(o2.type);
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        // xor components
+        return this.name.hashCode() ^ this.type.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return "<DBObject name=" + this.name + ", type=" + this.type +">";
     }
 
 }
